f27fc4f1345de1063ed3688d81dd93abfc9354e3
Allan Pedin <allan.pedin.17@cnu.edu>
2020-02-19 01:22:01 -0500

    dispatcher and stats implemented, needed more probs, no compile yet

--- dispatcher/dispatcher.cpp
+++ dispatcher/dispatcher.cpp
@@ -3,7 +3,21 @@
  *
  *  Created on: Sep 8, 2019
  *      Author: keith
+ *      Editor: Allan Pedin
  */
 
-//TODO fill in content
-
+#include "../includes/dispatcher.h"
+Dispatcher::~Dispatcher() {
+    delete  cpu;
+    delete is_valid_job_on_cpu;//not sure if needed
+}
+PCB Dispatcher::get_from_CPU() {
+    PCB retVal;//TODO
+    return retVal;
+}
+void Dispatcher::put_on_CPU(PCB &process) {
+    cpu->put_process_on_core(process);
+}
+bool Dispatcher::isValidJobOnCPU() {
+    return cpu->get_COPY_of_Current_Process().isEmpty();//Sounds right
+}
--- stats/stats.cpp
+++ stats/stats.cpp
@@ -3,6 +3,29 @@
  *
  *  Created on: Sep 10, 2019
  *      Author: keith
+ *      Editor: Allan Pedin
  */
 
-//TODO fill in content
+#include "../includes/stats.h"
+
+Stats::Stats(std::vector <PCB> &finished_vector):vex(&finished_vector) {};
+void Stats::showAllProcessInfo() {
+    for(PCB process : vec){
+        cout << "Process 1 Required CPU time:"<<process.required_cpu_time<<
+                "  arrived:"<<process.arrival_time<<
+                " started:"<<process.start_time<<
+                " finished:"<<process.finish_time;
+    }
+}
+float Stats::get_av_response_time() {
+    return av_response_time
+}
+float Stats::get_av_turnaround_time() {
+    return av_turnaround_time
+}
+float Stats::get_av_wait_time() {
+    return av_wait_time;
+}
+void Stats::calcStats() {
+    //TODO update wait time, turnaround time, response time
+}
